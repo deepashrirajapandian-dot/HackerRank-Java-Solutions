1923172e27489f561ecfc2ad56237165f7a3be65
deepashrirajapandian-dot <deepashrirajapandian@gmail.com>
2026-02-23 22:30:32 +0530

    Create static.java
    static initializer java program successfully executed in hackerank .

---
+++ static.java
@@ -0,0 +1,102 @@
+/*Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
+
+It's time to test your knowledge of Static initialization blocks. You can read about it here.
+
+You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth  and height . You should read the variables from the standard input.
+
+If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+Input Format
+
+There are two lines of input. The first line contains : the breadth of the parallelogram. The next line contains : the height of the parallelogram.
+
+Constraints
+
+Output Format
+
+If both values are greater than zero, then the main method must output the area of the parallelogram. Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+Sample input 1
+
+1
+3
+Sample output 1
+
+3
+Sample input 2
+
+-1
+2
+Sample output 2
+
+java.lang.Exception: Breadth and height must be positive
+Language
+Java 8
+More
+10111213141516171819202122232425897
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+}
+4public class Solution {
+Line: 27 Col: 2
+
+Test against custom input
+Java
+You have earned 10.00 points!
+You are now 62 points away from the 4th star for your java badge.
+11%88/150
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+
+Test case 6
+Compiler Message
+Success
+Input (stdin)
+1
+3
+Expected Output
+3*/
+import java.io.*;
+import java.util.*;
+
+public class static{
+
+    static int B;
+    static int H;
+    static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+}
